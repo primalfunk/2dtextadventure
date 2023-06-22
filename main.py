0c054e76bf405d7f5dc0241759a88e227bc7358e
Jared Menard <31257920+primalfunk@users.noreply.github.com>
2023-06-22 10:44:21 -0700

    Add files via upload
    First upload of busted code

---
+++ main.py
@@ -0,0 +1,11 @@
+from PySide6.QtWidgets import QApplication
+from game_gui import GameGUI, load_game_data
+
+def main():
+    app = QApplication([])
+    game_data = load_game_data('game_data.json')
+    game_gui = GameGUI(game_data)
+    app.exec()
+
+if __name__ == '__main__':
+    main()
